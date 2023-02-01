056328a8a6c8dd82051b805e6f27d3185c23054d
Michael Gatmaitan <mchlgtmtn@gmail.com>
2023-02-01 13:04:27 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import moment from 'moment';
+
+
 
 function App() {
+
+  let [time, setTime] = useState(moment().format('LTS'));
+
+  setInterval(() => setTime(moment().format('LTS')), 1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {time}
     </div>
   );
 }
